28b3fa706f1d14badc4e6d18e9ec86bb78f09d9f
Dan Smtih <dan.smith@kinandcarta.com>
2021-03-23 10:28:52 +0000

    Project setup

---
+++ WatchConnect/WatchConnect WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchConnect WatchKit Extension
+//
+//  Created by Dan Smith on 23/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchConnect/WatchConnect WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  WatchConnect WatchKit Extension
+//
+//  Created by Dan Smith on 23/03/2021.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
